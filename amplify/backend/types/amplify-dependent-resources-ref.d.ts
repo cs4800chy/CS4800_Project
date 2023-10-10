75ef3e465a2829a276dc210f8a362b0fb564e0ba
YsmaelK <99379297+YsmaelK@users.noreply.github.com>
2023-10-10 00:17:56 -0700

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "practice02cdb6bf": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
