47bd9ee7f440d1db19b8ef5463600cd436999f30
YsmaelK <99379297+YsmaelK@users.noreply.github.com>
2023-10-12 14:43:28 -0700

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 const Cart = ({ cart, onUpdateCartQuantity, onRemoveFromCart, onCheckout  }) => {
   if (!cart) {
@@ -10,11 +11,11 @@ const Cart = ({ cart, onUpdateCartQuantity, onRemoveFromCart, onCheckout  }) => 
       <h1>Your Shopping Cart</h1>
       {cart.line_items.map((item) => (
         <div key={item.id} className="cart-item">
+           
           <h3>{item.name}</h3>
           <p>Price: ${item.price.formatted}</p>
           <p>Quantity: {item.quantity}</p>
-          <button onClick={() => onUpdateCartQuantity(item.id, item.quantity - 1)}>-</button>
-          <button onClick={() => onUpdateCartQuantity(item.id, item.quantity + 1)}>+</button>
+          
           <button onClick={() => onRemoveFromCart(item.id)}>Remove</button>
         </div>
       ))}
@@ -22,7 +23,9 @@ const Cart = ({ cart, onUpdateCartQuantity, onRemoveFromCart, onCheckout  }) => 
         <h2>Cart Summary</h2>
         <p>Total Items: {cart.total_items}</p>
         <p>Total Price: ${cart.subtotal ? cart.subtotal.formatted : 0}</p> {/* Check if subtotal is defined */}
-        <button onClick={onCheckout}><a href="/checkout">Checkout</a></button> {/* Add the checkout button */}
+        {cart.total_items > 0 && ( // Add a conditional check here
+    <button onClick={onCheckout}><a href="/checkout">Checkout</a></button> 
+  )}
       </div>
     </div>
   );
