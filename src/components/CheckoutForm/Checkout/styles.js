354aac727878a5e914c0413a82e5740319d6c270
YsmaelK <99379297+YsmaelK@users.noreply.github.com>
2023-10-19 21:22:41 -0700

    Update styles.js

--- src/components/CheckoutForm/Checkout/styles.js
+++ src/components/CheckoutForm/Checkout/styles.js
@@ -1,40 +1,37 @@
-import {makeStyles} from '@mui/styles'
-
-export default makeStyles((theme) => ({
-  appBar: {
-    position: 'relative',
-  },
-  
-  layout: {
-    marginTop: '5%',
-    width: 'auto',
-    marginLeft: 2,
-    marginRight: 2,
-    
-  },
-  paper: {
-    marginTop: 3,
-    marginBottom: 3,
-    padding: 2,
-    
-  },
-  stepper: {
-    padding: 3
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  button: {
-    marginTop: 3,
-    marginLeft: 1,
-  },
-  divider: {
-    margin: '20px 0',
-  },
-  spinner: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
+export default function makeStyles(theme) {
+  return {
+    appBar: {
+      position: 'relative',
+    },
+    layout: {
+      marginTop: '5%',
+      width: 'auto',
+      marginLeft: 2,
+      marginRight: 2,
+    },
+    paper: {
+      marginTop: 3,
+      marginBottom: 3,
+      padding: 2,
+    },
+    stepper: {
+      padding: 3,
+    },
+    buttons: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    button: {
+      marginTop: 3,
+      marginLeft: 1,
+    },
+    divider: {
+      margin: '20px 0',
+    },
+    spinner: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  };
+}
